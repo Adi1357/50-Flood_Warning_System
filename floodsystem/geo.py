17c01994d803b34da1cf7783628eb984e293a026
Adi Jain <adi.jain1357@gmail.com>
2023-01-30 19:53:57 +0000

    Adi Task 1D

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -10,9 +10,10 @@ from utils import sorted_by_key  # noqa
 from haversine import haversine, Unit
 from stationdata import build_station_list
 from station import MonitoringStation
+from collections import defaultdict
 
 
-
+#Task 1B
 def stations_by_distance(stations, p):
     distance = []
 
@@ -23,7 +24,41 @@ def stations_by_distance(stations, p):
     return (sorted_by_key((distance),(2)))
 
 
-stations= build_station_list()
-print(stations_by_distance(stations, (52.2053, 0.1218)))
+
 
 #def stations_within_radius(stations, centre, r):
+
+
+#Task 1D
+def rivers_with_station(stations):
+    rivers = set()
+    for station in stations:
+        rivers.add(station.river)
+    rivers = sorted(rivers)
+    return print(f"{len(rivers)} stations. First 10 - [{rivers[slice(10)]}]")
+
+def stations_by_river(stations):
+    ret = {}
+    #for station in stations:
+     #   if station.river in ret:
+      #      ret[station.river].append(station.name)
+       # else:
+        #    ret[station.river] = [station.name]
+  
+
+    for station in stations:
+        if station.river not in ret:
+            ret[station.river] = [station.name]
+        else:
+            ret[station.river].append(station.name)
+            ret[station.river].sort()
+        
+    
+
+    return ret
+
+stations= build_station_list()
+station ={}
+
+print(stations_by_river(stations)['River Aire'])
+
