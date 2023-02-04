8e832d3dbeb36f34f5127dc478375ae6c5139445
Adi Jain <adi.jain1357@gmail.com>
2023-02-04 16:26:20 +0000

    Adi further update task 1F

---
+++ Task1F.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2018 Garth N. Wells
+#
+# SPDX-License-Identifier: MIT
+
+from floodsystem.stationdata import build_station_list
+from floodsystem.station import inconsistent_typical_range_stations
+
+def run():
+    """Requirements for Task 1F"""
+    stations = build_station_list()
+    inconsistent_typical_range_stations(stations)
+
+    
+if __name__ == "__main__":
+    print("*** Task 1F: CUED Part IA Flood Warning System ***")
+    run()
