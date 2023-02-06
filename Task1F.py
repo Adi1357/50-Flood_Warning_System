ec03ac177ba2cc86c7d824d33055d769102323b2
Adi Jain <adi.jain1357@gmail.com>
2023-02-06 08:04:27 +0000

    Adi further update task 1F

--- Task1F.py
+++ Task1F.py
@@ -8,7 +8,7 @@ from floodsystem.station import inconsistent_typical_range_stations
 def run():
     """Requirements for Task 1F"""
     stations = build_station_list()
-    inconsistent_typical_range_stations(stations)
+    print(inconsistent_typical_range_stations(stations))
 
     
 if __name__ == "__main__":
