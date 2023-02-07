0a1ae316d4f730b0708748b6a9d519846cd8dbff
Adi Jain <adi.jain1357@gmail.com>
2023-02-07 12:44:41 +0000

    Test on task 1E

--- Task1E.py
+++ Task1E.py
@@ -8,7 +8,7 @@ from floodsystem.geo import rivers_by_station_number
 def run():
     stations = build_station_list()
     print(rivers_by_station_number(stations, 9))
-
+    print(rivers_by_station_number(stations, 10))
 if __name__ == "__main__":
     print("*** Task 1E: CUED Part IB Flood Warning System ***")
     run()
