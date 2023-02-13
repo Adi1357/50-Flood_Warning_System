074cf56d33b316cf6c700725674df21ac0db72ca
Adi Jain <adi.jain1357@gmail.com>
2023-02-13 17:51:54 +0000

    Adi attempt 3 Task 2E

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,28 +1,29 @@
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
-
+from floodsystem.datafetcher import fetch_measure_levels
 
 def plot_water_levels(station, dates, levels):
+   
+   dt = 10
+   dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+
+   date_list=[]
+   level_list=[]
 
 
+   for date, level in zip(dates, levels):
+      date_list.append(date)
+      level_list.append(level)
 
-
-   # t = [datetime(2016, 12, 30), datetime(2016, 12, 31), datetime(2017, 1, 1),
-   #     datetime(2017, 1, 2), datetime(2017, 1, 3), datetime(2017, 1, 4),
-    #    datetime(2017, 1, 5)]
-    # level = [0.2, 0.7, 0.95, 0.92, 1.02, 0.91, 0.64]
-
-    # Plot
-    plt.plot(dates, levels[0])
-    plt.plot(dates, levels[1])
+   plt.plot(date_list, level_list)
 
     # Add axis labels, rotate date labels and add plot title
-    plt.xlabel('date')
-    plt.ylabel('water level (m)')
-    plt.xticks(rotation=45)
-    plt.title(station.name)
+   plt.xlabel('date')
+   plt.ylabel('water level (m)')
+   plt.xticks(rotation=45)
+   plt.title(station.name)
 
     # Display plot
-    plt.tight_layout()  # This makes sure plot does not cut off date labels
+   plt.tight_layout()  # This makes sure plot does not cut off date labels
 
-    plt.show()
+   plt.show()
