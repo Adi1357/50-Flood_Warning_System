ede41f6ba6a95477acf393ac5de6c0dbbeef313a
Adi Jain <adi.jain1357@gmail.com>
2023-02-13 19:12:14 +0000

    Adi change 2E

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 from floodsystem.datafetcher import fetch_measure_levels
 
-def plot_water_levels(station, dates, levels):
+def plot_water_levels(station, dt, levels):
    
-   dt = 10
+
    dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
 
    date_list=[]
