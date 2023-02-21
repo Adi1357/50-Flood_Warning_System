fb4379576397e699443faefca39844cfbde1e591
Adi Jain <adi.jain1357@gmail.com>
2023-02-21 18:31:03 +0000

    adi attempt task 2G

---
+++ Task2G.py
@@ -0,0 +1,15 @@
+from floodsystem.analysis import flood_warning  
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.flood import stations_highest_rel_level
+
+def run():
+    stations = build_station_list()
+    update_water_levels(stations)
+
+    print(flood_warning(stations, 10))
+    
+
+if __name__ == "__main__":
+    print("*** Task 2G: CUED Part IA Flood Warning System ***")
+    run()
+    
--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -1,9 +1,10 @@
 # Analysis
-# Jasper Hersov
 # 18/02/2023
-
+from floodsystem.datafetcher import fetch_measure_levels
 import numpy as np
 import matplotlib
+from floodsystem.datafetcher import fetch_measure_levels
+import datetime
 
 def polyfit(dates, levels, p):
     x = matplotlib.dates.date2num(dates)
@@ -11,4 +12,38 @@ def polyfit(dates, levels, p):
     p_coeff = np.polyfit(x-x[0], y, p)
     poly = np.poly1d(p_coeff)
     return poly, x[0]
+
+def flood_warning(stations,dt):
+    for station in stations:
+        dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+        date_list=[]
+        level_list=[]
+
+        for date, level in zip(dates, levels):
+            date_list.append(date)
+            level_list.append(level)
+        
+        sum_level = 0
+        counter = 0
+        sum_level = sum(list(level_list))
+
+        #for i in level_list:
+           # sum += level_list[i]
+        counter += 1
+
+        avg = sum_level/counter
+
+        if avg >= station.typical_range[1]:
+            print(station.name, 'Severe risk')
+
+        if avg >= 0.9*station.typical_range[1] and avg < station.typical_range[1]:
+            print(station.name, 'High risk')
+        
+        if avg <= station.typical_range[1] and avg >= station.typical_range[0]:
+            print(station.name, 'Moderate risk')
+
+
+        else:
+            print(station.name, 'Low risk')
+
 
